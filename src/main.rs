40c71147490e037bbb6d8c46eeeda04c0b97d6a6
Frank Waldheim <fw@123k.org>
2023-06-05 20:50:30 +0200

    badly working - chatGPT was no help with rust-rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use gtk::{Application, ApplicationWindow, Builder, EmojiChooser, glib, Label};
-use gtk::glib::{clone, MainContext};
+use gtk::{Application, ApplicationWindow, Builder, Button, EmojiChooser, glib};
+use gtk::glib::clone;
 use gtk::prelude::*;
 
 fn main() -> glib::ExitCode {
@@ -20,34 +20,20 @@ pub fn build_ui(application: &Application) {
 
     let window: ApplicationWindow = builder.object("window").expect("Couldn't get window");
     window.set_application(Some(application));
-    let emoji_label : Label= builder.object("emoji_label").expect("Couldn't get emoji_label");
-    let emoji_chooser : EmojiChooser= builder.object("emoji_chooser").expect("Couldn't get emoji_chooser");
-    emoji_label.set_text("(x)");
-    emoji_chooser.connect_emoji_picked(clone!(@weak emoji_label => move |_emoji_chooser, emoji| {
-        emoji_label.set_text(emoji);
-        println!("emoji {:#?}", emoji);
+    let emoji_button: Button = builder.object("emoji_button").expect("Couldn't get emoji_button");
+    emoji_button.set_label("😀");
+    let emoji_chooser: EmojiChooser = EmojiChooser::builder()
+        .build();
+
+    emoji_chooser.connect_emoji_picked(|_emoji_chooser, emoji| {
+        print!("{}", emoji);
+    });
+
+
+    emoji_button.set_child(Some(&emoji_chooser));
+    emoji_button.connect_clicked(clone!(@weak emoji_chooser => move |_| {
+        emoji_chooser.popup();
+        emoji_chooser.present();
     }));
-    // show the emoji chooser without blocking the main thread
-
-
-    // show a window with the emoji chooser
-
-
-
-
-
-    //let main_context = MainContext::default();
-    // The main loop executes the asynchronous block
-    // main_context.spawn_local(clone!(@weak emoji_chooser => async move {
-    //     emoji_chooser.show();
-    //     //emoji_chooser.present();
-    //     //emoji_chooser.popup();
-    //     println!("chooser {}", emoji_chooser);
-    //     let widget = emoji_chooser.default_widget();
-    //     println!("widget {:#?}", widget);
-    //     let pointing = emoji_chooser.pointing_to();
-    //     println!("pointing to {}", pointing.0);
-    // }));
-    emoji_chooser.show();
     window.present();
 }
