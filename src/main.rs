fd16104a6d1e64582a96423a0af80e68ea87b0a2
Frank Waldheim <fw@123k.org>
2023-06-05 18:15:38 +0200

    non working pre-chat-gpt bullshit (5h)

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,53 @@
+use gtk::{Application, ApplicationWindow, Builder, EmojiChooser, glib, Label};
+use gtk::glib::{clone, MainContext};
 use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow, Button, EmojiChooser};
-
-const APP_ID: &str = "org.gtk_rs.HelloWorld2";
 
 fn main() -> glib::ExitCode {
-    // Create a new application
-    let app = Application::builder().application_id(APP_ID).build();
-
-    // Connect to "activate" signal of `app`
-    app.connect_activate(build_ui);
-
-    // Run the application
-    app.run()
+    let application = Application::new(
+        Some("de.efwe.saeuep"),
+        Default::default(),
+    );
+    application.connect_activate(build_ui);
+    application.run()
 }
 
-fn build_ui(app: &Application) {
-    let emoji_chooser = EmojiChooser::builder()
-        .build();
+pub fn build_ui(application: &Application) {
+    let ui_src = include_str!("saeuep.ui");
+    let builder = Builder::new();
+    builder
+        .add_from_string(ui_src)
+        .expect("Could not read UI from 'saeuep.ui'");
 
-    let button = Button::builder()
-        .label("Press me!")
-        .child(&emoji_chooser)
-        .build();
+    let window: ApplicationWindow = builder.object("window").expect("Couldn't get window");
+    window.set_application(Some(application));
+    let emoji_label : Label= builder.object("emoji_label").expect("Couldn't get emoji_label");
+    let emoji_chooser : EmojiChooser= builder.object("emoji_chooser").expect("Couldn't get emoji_chooser");
+    emoji_label.set_text("(x)");
+    emoji_chooser.connect_emoji_picked(clone!(@weak emoji_label => move |_emoji_chooser, emoji| {
+        emoji_label.set_text(emoji);
+        println!("emoji {:#?}", emoji);
+    }));
+    // show the emoji chooser without blocking the main thread
 
 
-    // Connect to "clicked" signal of `button`
-    button.connect_clicked(|button| {
-        // Set the label to "Hello World!" after the button has been clicked on
-        button.set_label("Hello World!");
-        
-        //emoji_chooser.visible(true);
-    });
+    // show a window with the emoji chooser
 
-    // Create a window
-    let window = ApplicationWindow::builder()
-        .application(app)
-        .title("My GTK App")
-        .child(&button)
-        .build();
 
-    // Present window
+
+
+
+    //let main_context = MainContext::default();
+    // The main loop executes the asynchronous block
+    // main_context.spawn_local(clone!(@weak emoji_chooser => async move {
+    //     emoji_chooser.show();
+    //     //emoji_chooser.present();
+    //     //emoji_chooser.popup();
+    //     println!("chooser {}", emoji_chooser);
+    //     let widget = emoji_chooser.default_widget();
+    //     println!("widget {:#?}", widget);
+    //     let pointing = emoji_chooser.pointing_to();
+    //     println!("pointing to {}", pointing.0);
+    // }));
+    emoji_chooser.show();
     window.present();
 }
